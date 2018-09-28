399fe251699fe17dc27e92ccc6b2f57e1be60cf7
radomampianina <randriampenitra@yahoo.com>
2018-09-28 22:52:09 +0300

    mis à jour exo 17

--- exo_17.rb
+++ exo_17.rb
@@ -1,11 +1,14 @@
 puts "Veuillez entrer votre année de naissance : ex (1234)"
 print "> "
 age = gets.chomp.to_i
-ref = 2017 - age
 x = 0
-y = ref
-until x == ref
+y = 2018 - age  
+while y > 0 && x >= 0 do
     x += 1
     y -= 1
-    puts "Il y a #{x} an(s), tu avais #{y} ans"
-end  
+    if (x == y)
+        puts "Il y a #{x} ans, tu avais la moitié de l'age que tu as aujourd'hui"
+    elsif
+        puts "Il y a #{x} an(s), tu avais #{y} ans"
+    end
+end    
